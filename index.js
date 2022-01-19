5c7d5a9f5cc28171ccc093e843f1b5913115c542
Kevin Farias <kevin.farias.rib@gmail.com>
2022-01-19 09:38:59 -0300

    1

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const botaoMenu = document.querySelector('.cabecalho__menu');
-const menu = document.querySelector('.menu-lateral');
-
+const botaoMenu = document.querySelector('.cabecalho__menu')
+const menu = document.querySelector('.menu-lateral')
 
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo')
